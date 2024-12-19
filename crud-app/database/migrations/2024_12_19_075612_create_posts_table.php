ee6ea38f3ac44d88b9062f82166b4fd89edd4ebe
quan9120012 <xb91200@gmail.com>
2024-12-19 15:33:44 +0700

    Database Migrations

---
+++ crud-app/database/migrations/2024_12_19_075612_create_posts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('desc');
+            $table->string('status');
+            $table->string('img_url')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
